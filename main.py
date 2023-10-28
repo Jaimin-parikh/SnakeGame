25fb8e5ba3e21680548687e36256cbb37823881e
Jaimin-parikh <jaiminparikh12@gmail.com>
2023-10-28 20:47:03 +0530

    Creating Snake and moving it

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 from turtle import Screen
+from snake import Snake
+import time
+
+screen = Screen()
+screen.tracer(0)
+
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+
+
+game_flag = True
+snake = Snake()
+
+while game_flag:
+    snake.move()
+    screen.update()
+    time.sleep(0.5)
+
+
+screen.exitonclick()
--- snake.py
+++ snake.py
@@ -1,5 +1,24 @@
 from turtle import Turtle
 
+POSITIONS = [(0, 0), (20, 0), (40, 0)]
+
 
 class Snake:
     def __init__(self):
+        self.segments = []
+        for position in POSITIONS:
+            self.create_snake(position)
+        self.head = self.segments[0]
+
+    def create_snake(self, position):
+        t = Turtle(shape="square")
+        t.pu()
+        t.color("white")
+        t.setpos(position)
+        self.segments.append(t)
+
+    def move(self):
+        for i in range(len(self.segments)-1, 0, -1):
+            new_position = self.segments[i-1].pos()
+            self.segments[i].goto(new_position)
+        self.head.fd(20)
